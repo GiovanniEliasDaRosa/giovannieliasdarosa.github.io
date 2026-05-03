9552af7b1851def8b924d2a1085ede3963815d64
GiovanniBolivoro <101050472+GiovanniEliasDaRosa@users.noreply.github.com>
2026-05-03 20:57:37 -0300

    feat(hero): add Hero component
    Create Hero component and module styles

---
+++ new-portifolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,32 @@
+import styles from "./Hero.module.css";
+
+export default function Hero() {
+  return (
+    <section className={`${styles.hero} full_width`}>
+      <img src={`${import.meta.env.BASE_URL}assets/images/background.jpg`} alt="Coding" />
+
+      <h1>
+        <span>A passionate web developer</span>
+        <span className={styles.hero_blinker}></span>
+      </h1>
+
+      <a
+        href="#skills"
+        className="no_default_styles icons nomargin chevron_down"
+        id={styles.go_down}
+        aria-label="Go to skills"
+      ></a>
+
+      <p>
+        Photo by
+        <a href="https://unsplash.com/@krishna2803?utm_content=creditCopyText&amp;utm_medium=referral&amp;utm_source=unsplash">
+          Krishna Pandey
+        </a>
+        on
+        <a href="https://unsplash.com/photos/text-KNZHyTpre18?utm_content=creditCopyText&amp;utm_medium=referral&amp;utm_source=unsplash">
+          Unsplash
+        </a>
+      </p>
+    </section>
+  );
+}
