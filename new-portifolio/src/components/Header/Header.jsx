6474b18edca78977bbc6672c9efed0e7536bf8e7
GiovanniBolivoro <101050472+GiovanniEliasDaRosa@users.noreply.github.com>
2026-05-13 21:55:22 -0300

    feat(header): add responsive hamburger menu
    Implement hamburger menu component in header shown in smaller screens
    Add CSS animation for open and close transitions

--- new-portifolio/src/components/Header/Header.jsx
+++ new-portifolio/src/components/Header/Header.jsx
@@ -1,14 +1,106 @@
+import { useRef, useState } from "react";
 import styles from "./Header.module.css";
 
 export default function Header() {
+  const [mobile, setMobile] = useState(true);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef();
+
+  function toggleMenu() {
+    if (!menuOpen) {
+      // If not open, open it
+      setMenuOpen(true);
+
+      requestAnimationFrame(() => {
+        menuRef.current?.classList.add(styles.show);
+      });
+    } else {
+      // If open, close it
+      const element = menuRef.current;
+
+      if (!element) return setMenuOpen(false);
+
+      element.classList.remove(styles.show);
+      element.classList.add(styles.hide);
+
+      element.addEventListener(
+        "animationend",
+        () => {
+          setMenuOpen(false);
+        },
+        { once: true },
+      );
+    }
+  }
+
+  useState(() => {
+    let timeoutUpdateHeader = null;
+
+    function updateHeader() {
+      const fontSize = parseFloat(window.getComputedStyle(document.documentElement).fontSize);
+      const isMobile = window.innerWidth < fontSize * 43;
+
+      setMobile((prevMobile) => {
+        if (prevMobile === isMobile) return prevMobile;
+
+        return isMobile;
+      });
+    }
+
+    function debounceHeader() {
+      clearTimeout(timeoutUpdateHeader);
+
+      timeoutUpdateHeader = setTimeout(updateHeader, 150);
+    }
+
+    window.addEventListener("resize", debounceHeader);
+    updateHeader();
+
+    return () => {
+      window.removeEventListener("resize", debounceHeader);
+    };
+  }, []);
+
   return (
-    <header className={styles.header}>
-      <nav>
-        <div className={styles.header_logo}>
-          <img src={`${import.meta.env.BASE_URL}assets/images/logo.png`} alt="" />
-          <p>Giovanni's Portfolio</p>
-        </div>
-        <div className={styles.header_links}>
+    <>
+      <header className={styles.header}>
+        <nav>
+          <div className={styles.header_logo}>
+            <img src={`${import.meta.env.BASE_URL}assets/images/logo.png`} alt="" />
+            <p>Giovanni's Portfolio</p>
+          </div>
+
+          {mobile ? (
+            // Show menu button on nav
+            <button
+              className="square icons nomargin bars"
+              aria-label="Open menu"
+              onClick={toggleMenu}
+            ></button>
+          ) : (
+            // Show buttons
+            <div className={styles.header_links}>
+              <a className="button" href="#skills">
+                Skills
+              </a>
+              <a className="button" href="#projects">
+                Projects
+              </a>
+              <a className="button" href="#contact">
+                Contact
+              </a>
+            </div>
+          )}
+        </nav>
+      </header>
+
+      {mobile && menuOpen ? (
+        <div ref={menuRef} className={`${styles.header_links} ${styles.header_mobile}`}>
+          <button
+            className={`square icons nomargin xmark ${styles.close_button}`}
+            aria-label="Close menu"
+            onClick={toggleMenu}
+          ></button>
           <a className="button" href="#skills">
             Skills
           </a>
@@ -19,7 +111,7 @@ export default function Header() {
             Contact
           </a>
         </div>
-      </nav>
-    </header>
+      ) : null}
+    </>
   );
 }
