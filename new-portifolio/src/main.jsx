2e39cdd3531a9969b178d65fac4ab45f33c2bf6b
GiovanniBolivoro <101050472+GiovanniEliasDaRosa@users.noreply.github.com>
2026-06-03 12:21:09 -0300

    fix(seo): fix SEO tags

--- new-portifolio/src/main.jsx
+++ new-portifolio/src/main.jsx
@@ -2,9 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.jsx";
-import { setRuntimeMeta } from "./meta";
-
-setRuntimeMeta();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
