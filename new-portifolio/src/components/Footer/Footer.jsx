8831808fedbc3699ebaac61de7856497bc5f4e30
Giovanni <101050472+GiovanniEliasDaRosa@users.noreply.github.com>
2026-06-19 19:46:22 -0300

    fix(footer): improve footer text
    Change footer text
    Add easter egg animation to React logo on footer

--- new-portifolio/src/components/Footer/Footer.jsx
+++ new-portifolio/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <p>
-        Made by me with love and
+        Made with
         <span className={`icons nomargin brands react ${styles.react}`}>React</span>
       </p>
     </footer>
